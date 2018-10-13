00f6b6c711398e3b27f5793cc6cc35379ac84733
kendell harris <kendell.harris@hgs.hiddengeniusproject.org>
2018-10-12 18:58:02 -0700

    first commit

---
+++ download.sh
@@ -0,0 +1,15 @@
+cd ~/desktop
+mkdir week4-wed
+cd week4-wed
+touch index.html
+mkdir css
+mkdir img
+cd css 
+touch style1.css
+touch style2.css
+cd ../
+cd img 
+curl -o img1.jpg "http://d1nr5wevwcuzuv.cloudfront.net/product_photos/17382209/375_400w.jpg"
+curl -o img2.jpg "http://d111vui60acwyt.cloudfront.net/product_photos/25742679/image_400w.jpg"
+curl -o img3.jpg "https://static.highsnobiety.com/wp-content/uploads/2018/07/21131849/nike-air-jordan-11-concord-2018-release-date-price-01-960x640.jpg"
+cd ../
